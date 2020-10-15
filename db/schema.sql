161fd0344382695e733e449facab62fda71b1b8d
doh <theshawns0n@gmail.com>
2020-10-14 23:13:32 -0400

    schema change

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
-
+USE burgers_db;
 CREATE TABLE burgers(
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     burger_name varchar(30),
     devoured BOOLEAN default false
-)
+);
 
