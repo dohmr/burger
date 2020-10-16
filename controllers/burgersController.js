cb491a42a5c636cb1f11a69067c88305c7aee729
doh <theshawns0n@gmail.com>
2020-10-15 22:48:42 -0400

    pseudo for our ROUTES requiring ORM

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,5 +1,14 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+// const exphbs = require("express-handlebars");
 const app = express();
 
-const burgers = require("../models/burger.js")
+const burgers = require("../models/burgers.js")
+
+// express.Router to create modular, mountable route handlers.
+const router = express.Router();
+
+// GET route to retrieve data from DB
+
+// add a new burger and POST it into our DB
+
+// update our burger, PUT its 'status' to devoured by :id
